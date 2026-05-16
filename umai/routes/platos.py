2014d3c0ad2c0867ae4de681bd249fd57234b51f
Cami-loiacono <camiloia.cole@gmail.com>
2026-05-16 02:15:40 -0300

    feat: Implementacion endpoint de listado de platos

---
+++ umai/routes/platos.py
@@ -0,0 +1,19 @@
+
+from flask import Blueprint, jsonify, request
+
+import logging
+from umai.utils import construir_error_api
+from umai.services.platos import traer_todos_los_platos
+
+platos_bp = Blueprint('platos', __name__)
+
+@platos_bp.route('/', methods=['GET'])
+def listar_platos():
+    try:
+        platos = traer_todos_los_platos()
+        return jsonify({'data': platos, 'status': 'success'}), 200
+    
+    except Exception:
+        return jsonify(construir_error_api('LIST_ERROR', 'Error listando platos', 'Error inesperado')), 500
+
+
---
+++ umai/services/platos.py
@@ -0,0 +1,15 @@
+
+import logging
+from db import supabase
+
+def traer_todos_los_platos():
+    try:
+        platos_resp = (
+            supabase.table('platos')
+            .select('*')
+            .execute()
+        )
+        return platos_resp.data
+
+    except Exception:
+        return None

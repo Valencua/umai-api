6b6df2cfc018218a388075d52fa2c8d5cd450ff0
Valencua <valencuavasula@gmail.com>
2026-05-17 18:39:27 +0100

    Endpoint actualizar visita por uuid

--- umai/routes/reservas.py
+++ umai/routes/reservas.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify, request
 
-from umai.constants import ERROR_CODE_INVALID_BODY, ERROR_CODES_CONFLICTO, ERROR_CODE_INTERNAL_SERVER
-from umai.services.reservas import crear_reserva
+from umai.constants import ERROR_CODE_INVALID_BODY, ERROR_CODES_CONFLICTO, ERROR_CODE_INTERNAL_SERVER, ERROR_CODE_RESERVA_NO_ENCONTRADA
+from umai.services.reservas import crear_reserva, confirmar_asistencia_por_codigo
 from umai.utils import construir_error_api
-from umai.validators.reservas import validar_crear_reserva
+from umai.validators.reservas import validar_crear_reserva, validar_uuid_codigo
 
 reservas_bp = Blueprint('reservas', __name__)
 
@@ -34,4 +34,25 @@ def post_reserva():
             message='error al procesar la solicitud',
             description='Hubo un error interno'
         )), 500
-    
+
+
+@reservas_bp.route('/codigo/<uuid_codigo>', methods=['PATCH'])
+def patch_confirmar_asistencia(uuid_codigo):
+    try:
+        codigo = validar_uuid_codigo(uuid_codigo)
+        reserva = confirmar_asistencia_por_codigo(codigo)
+        return jsonify(reserva), 200
+    except ValueError as e:
+        error = e.args[0]
+        codigo_error = error['errors'][0]['code']
+        if codigo_error == ERROR_CODE_RESERVA_NO_ENCONTRADA:
+            return jsonify(error), 404
+        if codigo_error in ERROR_CODES_CONFLICTO:
+            return jsonify(error), 409
+        return jsonify(error), 400
+    except Exception:
+        return jsonify(construir_error_api(
+            code=ERROR_CODE_INTERNAL_SERVER,
+            message='error al procesar la solicitud',
+            description='Hubo un error interno'
+        )), 500

9f89cbfed69310ae37789dc193e6bf48e78a7a1c
Cami-loiacono <you@example.com>
2026-05-17 10:42:22 -0300

    fix: mejora de nombres de funciones y legibilidad del código de reservas

--- umai/routes/reservas.py
+++ umai/routes/reservas.py
@@ -1,18 +1,31 @@
 from flask import Blueprint, jsonify
 
 from umai.utils import construir_error_api
-from umai.services.reservas import traer_3_mas_recientes_reservas
+from umai.services.reservas import get_top3_reservas
 
 reservas_bp = Blueprint('reservas', __name__)
 
 @reservas_bp.route('/', methods=['GET'])
 def get_3_reservas():
     try:
-        reservas = traer_3_mas_recientes_reservas()
+        reservas = get_top3_reservas()
         if(reservas is None):
-            return jsonify(construir_error_api('DB_ERROR', 'Error al acceder a la base de datos', 'No se pudieron obtener las reservas')), 500
-        return jsonify({'data': reservas, 'status': 'success'}), 200
+            return jsonify(construir_error_api(
+                'DB_ERROR', 
+                'Error al acceder a la base de datos', 
+                'No se pudieron obtener las reservas')
+            ), 500
+
+        return jsonify(
+            {
+                'data': reservas, 
+                'status': 'success'}
+            ), 200
     
     except Exception:
-        return jsonify(construir_error_api('LIST_ERROR', 'Error listando las 3 reservas más recientes', 'Error inesperado')), 500
+        return jsonify(construir_error_api(
+            'LIST_ERROR', 
+            'Error listando las 3 reservas más recientes', 
+            'Error inesperado')
+        ), 500
     
--- umai/services/reservas.py
+++ umai/services/reservas.py
@@ -3,16 +3,12 @@ from db import supabase
 
 logger = logging.getLogger(__name__)
 
-def traer_3_mas_recientes_reservas():
-    try:
-        reservas_recientes = (
+def get_top3_reservas():
+    reservas_recientes = (
         supabase.table('reservas')
         .select('*')
         .order('reserva_id', desc=True) # obtener los ultimos id creados, es decir, las reservas mas recientes
         .limit(3)
         .execute()
-        )
-        return reservas_recientes.data
-
-    except Exception as e:
-        return None
+    )
+    return reservas_recientes.data

0f180c647677f63ef3d2270d45086538318b7874
mateo <msantalla@fi.uba.ar>
2026-05-19 00:16:06 -0300

    feat: funcionalidad obetener reservas- semanal

--- umai/routes/metricas.py
+++ umai/routes/metricas.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from umai.services.metricas import obtener_rating_promedio
+from umai.services.metricas import obtener_rating_promedio, obtener_reservas_semanal
 from umai.utils import construir_error_api
 from umai.constants import ERROR_CODE_INTERNAL_SERVER
 
@@ -17,3 +17,20 @@ def get_rating_promedio():
         )), 500
 
     return jsonify(metricas), 200
+
+@metricas_bp.route('/reservas-semanal', methods=['GET'])
+def get_reservas_semanal():
+    try:
+        reservas = obtener_reservas_semanal() 
+    except Exception as e:
+
+        import traceback
+        traceback.print_exc()
+        
+        return jsonify(construir_error_api(
+            code=ERROR_CODE_INTERNAL_SERVER,
+            message='error al procesar la solicitud',
+            description='Hubo un error interno'
+        )), 500
+
+    return jsonify({'data': reservas, 'status': 'success'}), 200
--- umai/services/metricas.py
+++ umai/services/metricas.py
@@ -1,4 +1,7 @@
 from db.supabase_client import supabase
+from datetime import datetime, timedelta, timezone
+from umai.constants import ESTADO_RESERVA_CONFIRMADO, FORMATO_FECHA
+from umai.validators import reservas
 
 def obtener_rating_promedio() -> dict:
     response = supabase.table('reseñas') \
@@ -14,3 +17,21 @@ def obtener_rating_promedio() -> dict:
     promedio = round(sum(r['rating'] for r in reseñas) / len(reseñas), 1)
 
     return {'promedio': promedio}
+
+def obtener_reservas_semanal() -> list:
+    hoy = datetime.now(timezone.utc)
+    hace_siete_dias = hoy - timedelta(days=7)
+
+    hoy_str = hoy.strftime(FORMATO_FECHA)
+    hace_siete_dias_str = hace_siete_dias.strftime(FORMATO_FECHA)
+
+    response = (
+        supabase.table('reservas') 
+        .select('*') 
+        .eq('estado', ESTADO_RESERVA_CONFIRMADO) 
+        .gte('fecha', hace_siete_dias_str) 
+        .lte('fecha', hoy_str)
+        .execute()
+    )
+
+    return response.data if response.data else []

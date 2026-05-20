e5b699f6e4876f62bae3572752c86e40954c0c74
Valencua <valencuavasula@gmail.com>
2026-05-20 05:31:11 +0100

    minor fixes eliminacion endpoint no funcional

--- umai/routes/metricas.py
+++ umai/routes/metricas.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from umai.services.metricas import obtener_rating_promedio, obtener_reservas_hoy, obtener_cancelaciones_hoy, obtener_metricas_reservas, obtener_personas_hoy, obtener_reservas_semanal
+from umai.services.metricas import obtener_rating_promedio, obtener_reservas_hoy, obtener_cancelaciones_hoy, obtener_metricas_reservas, obtener_personas_hoy
 from umai.utils import construir_error_api
 from umai.constants import ERROR_CODE_INTERNAL_SERVER
 
@@ -80,21 +80,4 @@ def get_personas_hoy():
             description='Ocurrió un error al intentar obtener las personas para hoy'
         )), 500
 
-
-@metricas_bp.route('/reservas-semanal', methods=['GET'])
-def get_reservas_semanal():
-    try:
-        reservas = obtener_reservas_semanal() 
-    except Exception as e:
-
-        import traceback
-        traceback.print_exc()
-        
-        return jsonify(construir_error_api(
-            code=ERROR_CODE_INTERNAL_SERVER,
-            message='error al procesar la solicitud',
-            description='Hubo un error interno'
-        )), 500
-
-    return jsonify({'data': reservas, 'status': 'success'}), 200
 

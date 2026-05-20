fd208362205b8101f211c1849353de32904ca7e5
Valencua <valencuavasula@gmail.com>
2026-05-20 18:20:04 +0100

    merge: develop

--- umai/routes/platos.py
+++ umai/routes/platos.py
@@ -1,9 +1,68 @@
-from flask import Blueprint, jsonify
-from umai.services.platos import eliminar_plato
-from umai.utils import construir_error_api, validar_entero
-from umai.constants import ERROR_CODE_INTERNAL_SERVER
+from flask import Blueprint, jsonify, request
+from umai.services.platos import eliminar_plato, traer_todos_los_platos, crear_plato
+from umai.utils import construir_error_api, validar_entero, validar_crear_plato
 
-platos_bp = Blueprint('platos', __name__)
+from umai.constants import (
+    ERROR_CODES_CONFLICTO,
+    ERROR_CODE_INTERNAL_SERVER
+)
+
+
+platos_bp = Blueprint('platos', name)
+
+
+@platos_bp.route('/', methods=['POST'])
+def post_plato():
+
+    body = request.form.to_dict()
+
+    body['foto'] = request.files.get('foto')
+
+    try:
+
+        data = validar_crear_plato(body)
+
+        plato = crear_plato(data)
+
+        return jsonify(plato), 201
+
+    except ValueError as e:
+
+        error = e.args[0]
+
+        if isinstance(error, dict) and error.get('errors'):
+
+            if error['errors'][0]['code'] in ERROR_CODES_CONFLICTO:
+
+                return jsonify(error), 409
+
+            return jsonify(error), 400
+
+        return jsonify({'errors': error}), 400
+
+    except Exception as e:
+
+        return jsonify(construir_error_api(
+        code=ERROR_CODE_INTERNAL_SERVER,
+        message='error al procesar la solicitud',
+        description=str(e)
+    )), 500
+
+@platos_bp.route('/', methods=['GET'])
+def listar_platos():
+    try:
+        platos = traer_todos_los_platos()
+        return jsonify({
+            'data': platos,
+            'status': 'success'
+        }), 200
+
+    except Exception:
+        return jsonify(construir_error_api(
+            'LIST_ERROR',
+            'Error listando platos',
+            'Error inesperado')
+        ), 500
 
 @platos_bp.route('/<string:plato_id>', methods=['DELETE'])
 def delete_plato(plato_id):
--- umai/services/platos.py
+++ umai/services/platos.py
@@ -1,6 +1,126 @@
 from db import supabase
+import uuid
 from umai.utils import construir_error_api
 
+from umai.constants import (
+    ERROR_CODE_ETIQUETAS_INVALIDAS,
+    ERROR_CODE_PLATO_DUPLICADO
+)
+
+
+def crear_plato(data: dict) -> dict:
+
+    existente = (
+        supabase
+        .table('platos')
+        .select('plato_id')
+        .eq('nombre', data['nombre'])
+        .execute()
+    )
+
+    if existente.data:
+
+        raise ValueError(construir_error_api(
+            code=ERROR_CODE_PLATO_DUPLICADO,
+            message='Plato ya existente',
+            description=f"Ya existe un plato con el nombre '{data['nombre']}'"
+        ))
+
+    etiquetas = data.get('etiquetas', [])
+
+    if etiquetas:
+
+        etiquetas_existentes = (
+            supabase
+            .table('etiquetas')
+            .select('etiqueta_id')
+            .in_('etiqueta_id', etiquetas)
+            .execute()
+        )
+
+        ids_existentes = [
+            etiqueta['etiqueta_id']
+            for etiqueta in etiquetas_existentes.data
+        ]
+
+        etiquetas_invalidas = [
+            etiqueta_id
+            for etiqueta_id in etiquetas
+            if etiqueta_id not in ids_existentes
+        ]
+
+        if etiquetas_invalidas:
+
+            raise ValueError(construir_error_api(
+                code=ERROR_CODE_ETIQUETAS_INVALIDAS,
+                message='Etiquetas inexistentes',
+                description=f'Las siguientes etiquetas no existen: {etiquetas_invalidas}'
+            ))
+
+    foto = data['foto']
+
+    extension = foto.filename.split('.')[-1]
+
+    nombre_archivo = f'{uuid.uuid4()}.{extension}'
+
+    contenido_imagen = foto.read()
+
+    supabase.storage.from_('platos').upload(
+        path=nombre_archivo,
+        file=contenido_imagen,
+        file_options={
+            'content-type': foto.content_type
+        }
+    )
+
+    foto_url = (
+        supabase
+        .storage
+        .from_('platos')
+        .get_public_url(nombre_archivo)
+    )
+
+    response = (
+        supabase
+        .table('platos')
+        .insert({
+            'nombre': data['nombre'],
+            'descripcion': data['descripcion'],
+            'precio': data['precio'],
+            'foto': foto_url,
+        })
+        .execute()
+    )
+
+    plato = response.data[0]
+
+    if etiquetas:
+
+        relaciones = [
+            {
+                'plato_id': plato['plato_id'],
+                'etiqueta_id': etiqueta_id,
+            }
+            for etiqueta_id in etiquetas
+        ]
+
+        (
+            supabase
+            .table('plato_etiquetas')
+            .insert(relaciones)
+            .execute()
+        )
+
+    return plato
+
+def traer_todos_los_platos():
+    platos_resp = (
+        supabase.table('platos')
+        .select('*')
+        .execute()
+    )
+    return platos_resp.data
+    
 def eliminar_plato(plato_id: int) -> None:
 
     existente = supabase.table('platos') \

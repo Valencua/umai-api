7791fc0af5d12fbe473e642cd46934682f273efa
Valencua <valencuavasula@gmail.com>
2026-05-22 23:58:12 +0100

    minor_fix_merge

--- umai/routes/metricas.py
+++ umai/routes/metricas.py
@@ -12,69 +12,7 @@ def get_dashboard():
     except Exception as e:
         return jsonify(construir_error_api(
             code=ERROR_CODE_INTERNAL_SERVER,
-<<<<<<< HEAD
-            message='error al procesar la solicitud',
-            description='Hubo un error interno'
-        )), 500
-    return jsonify(metricas), 200
-
-
-@metricas_bp.route('/reservas-hoy', methods=['GET'])
-def get_reservas_hoy():
-    try:
-        reservas_cantidad = obtener_reservas_hoy()
-    except Exception:
-        return jsonify(construir_error_api(
-            code=ERROR_CODE_INTERNAL_SERVER,
-            message='Error al obtener reservas',
-            description='Ocurrió un error al intentar obtener las reservas para hoy'
-=======
             message='Error al obtener el dashboard',
             description='Ocurrió un error interno'
->>>>>>> develop-metricas
         )), 500
-    return jsonify(reservas_cantidad), 200
-
-
-<<<<<<< HEAD
-@metricas_bp.route('/cancelaciones-hoy', methods=['GET'])
-def get_cancelaciones_hoy():
-    try:
-        cancelaciones = obtener_cancelaciones_hoy()
-    except Exception as e:
-        return jsonify(construir_error_api(
-            code=ERROR_CODE_INTERNAL_SERVER,
-            message='error al procesar la solicitud',
-            description='Hubo un error interno'
-        )), 500
-    return jsonify(cancelaciones), 200
-
-@metricas_bp.route('/reservas', methods=['GET'])
-def get_metricas_reservas():
-    try:
-        metricas = obtener_metricas_reservas()
-    except Exception as e:
-        return jsonify(construir_error_api(
-            code=ERROR_CODE_INTERNAL_SERVER,
-            message='error al procesar la solicitud',
-            description='Hubo un error interno'
-        )), 500
-        
-    return jsonify(metricas), 200
-
-@metricas_bp.route('/personas-hoy', methods=['GET'])
-def get_personas_hoy():
-    try:
-        personas_cantidad = obtener_personas_hoy()
-    except Exception as e:
-        return jsonify(construir_error_api(
-            code=ERROR_CODE_INTERNAL_SERVER,
-            message='Error al obtener personas',
-            description='Ocurrió un error al intentar obtener las personas para hoy'
-        )), 500
-    return jsonify(personas_cantidad), 200
-
-
-=======
     return jsonify(dashboard), 200
->>>>>>> develop-metricas
